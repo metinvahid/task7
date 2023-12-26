91745f151a4c54cedc76d16147fb1c252948b042
metinvahid <matinav@code.edu.az>
2023-12-26 18:30:04 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+
+        }
+
+        static void Task1() 
+        {
+
+        }
+
+        static void Task2()
+        {
+
+        }
+
+
+
+    }
+}
